ab2e8391164501d968df6cafb422be0020a15e0b
rytjr <kks007159@naver.com>
2024-05-30 09:42:09 +0900

    time complexity

---
+++ src/AlgorethonClass1/AlgorethonClass1.java
@@ -0,0 +1,11 @@
+package AlgorethonClass1;
+
+public class AlgorethonClass1 {
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.print(0);
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module AlgorethonClass1 {
+}
